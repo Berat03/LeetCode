a7fe40389b543fa6d48e33b080d0ed2be4e94dd0
berat03 <bbulbulwork@gmail.com>
2023-04-19 02:32:51 +0100

    explore defaultdicts, can also adjust normal dicts

---
+++ Arrays&Hashings/36_Valid_Sudoku.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+# defaultdict can key set, list or int (0) argument
+def isValidSudoku(board):
+    rows = defaultdict(set)
+    cols = defaultdict(set)
+    squares = defaultdict(set)
+
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == ".":
+                continue
+            if board[r][c] in rows[r] or board[r][c] in cols[r] or board[r][c] in squares[(r//3, c//3)]:
+                return False
+            cols[c].add(board[r][c])
+            rows[r].add(board[r][c])
+            squares[(r // 3, c // 3)].add(board[r//3][c//3])
+
+    return True
+
