b9014e76f90e918579787f1fba9c3a1089b8539a
berat03 <bbulbulwork@gmail.com>
2023-04-22 14:29:15 +0100

    can do normally but not with bit manipulation

--- Bit_Manipulation/136_Single_Number.py
+++ Bit_Manipulation/136_Single_Number.py
@@ -10,17 +10,3 @@ def singleNumber(self, nums):
             numbers.pop(num)
     x = numbers.keys()
     return x[0]
-
-
-def singleNumber(self, nums):
-    numbers = dict()
-
-    for num in nums:
-
-        if not (num in numbers.keys()):
-            numbers[num] = 1
-
-        elif num in numbers.keys():
-            numbers.pop(num)
-    x = numbers.keys()
-    return x[0]
