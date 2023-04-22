014aed78a70ca087186656f962dd5920ed5e05d0
berat03 <bbulbulwork@gmail.com>
2023-04-22 14:09:30 +0100

    don't really grasp these solutions

---
+++ Linked_Lists/206_Reverse_Linked_List.py
@@ -0,0 +1,42 @@
+def reverseList(head):
+    prev, curr = None, head
+
+    while curr:
+        # temp
+        nxt = curr.next
+
+        # second value is going to become new prev
+        curr.next = prev
+        # shift pointer across,
+        prev = curr
+        curr = nxt
+    return prev
+
+def reverseList_recursive(self, head):
+    if not head: #if
+        return None
+
+    newHead = head
+
+    if head.next:
+        newHead = self.reverseList_recursive(head.next)
+        head.next.next = head
+    head.next = None
+    return newHead
+
+    # head = (1) head.next = None, so no change
+  """
+  Head = (1,2)
+  NewHead = (1,2)
+  head.next = 2
+    newHead = reverselist(2)
+        head.next = None
+        head.next = None
+        head(1 -> 2 -> None)
+...
+
+    Don't understand the recursive function, ask Nicky?
+    
+        
+  
+  """
