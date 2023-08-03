68918310922f171b2bd678a080690a38b64248ff
berat03 <bbulbulwork@gmail.com>
2023-08-03 22:16:32 +0100

    hash map solution

--- Arrays&Hashings/244_Valid_Anagram.py
+++ Arrays&Hashings/244_Valid_Anagram.py
@@ -2,3 +2,13 @@ def isAnagram(s, t):
     return sorted(s) == sorted(t)
 
 # Sorting in Python is O(nlogn)
+
+def isAnagram(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+
+    dic_s, dic_t = {}, {}
+    for i in range(len(s)):
+        dic_s[s[i]] = 1 + dic_s.get(s[i], 0)
+        dic_t[t[i]] = 1 + dic_t.get(t[i], 0)
+    return dic_s == dic_t
